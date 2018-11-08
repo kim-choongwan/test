a47bbe35625aa4846ed3e483f11742c02d539842
kim.choong.wan <kim.choong.wan@gmail.com>
2018-11-08 11:20:41 +0900

    [#7] 정적컨텐츠 위치 설정

---
+++ src/main/java/com/nudo/gg/cmm/config/WebConfig.java
@@ -0,0 +1,30 @@
+package com.nudo.gg.cmm.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class WebConfig implements WebMvcConfigurer{
+
+	/**
+	 * 정적인 컨텐츠가 좋을 위치
+	 */
+	private static final String[] CLASSPATH_RESOURCE_LOCATIONS = { "classpath:/static/" };
+
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		
+		if (!registry.hasMappingForPattern("/webjars/**")) {
+			registry.addResourceHandler("/webjars/**").addResourceLocations(
+					"classpath:/META-INF/resources/webjars/");
+		}
+		if (!registry.hasMappingForPattern("/**")) {
+			registry.addResourceHandler("/**").addResourceLocations(CLASSPATH_RESOURCE_LOCATIONS);
+		}
+		
+	}
+
+}
