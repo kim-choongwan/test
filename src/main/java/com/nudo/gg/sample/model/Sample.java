b989448b077673f3f3cd98f1f5ab2ac11beb385f
kim.choong.wan <kim.choong.wan@gmail.com>
2018-11-09 09:13:27 +0900

    [#10] 롬북 추가

--- src/main/java/com/nudo/gg/sample/model/Sample.java
+++ src/main/java/com/nudo/gg/sample/model/Sample.java
@@ -2,37 +2,16 @@ package com.nudo.gg.sample.model;
 
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
 
+@Getter @Setter @ToString 
 @ApiModel(value="샘플 VO", description="코멘트는 여기에 단다.")
 public class Sample {
 	
 	private int ID;
 	private int VAL;
 	private String TYPE;
-
-
-	public int getID() {
-		return ID;
-	}
-	public void setID(int iD) {
-		ID = iD;
-	}
-	public int getVAL() {
-		return VAL;
-	}
-	public void setVAL(int vAL) {
-		VAL = vAL;
-	}
-	public String getTYPE() {
-		return TYPE;
-	}
-	public void setTYPE(String tYPE) {
-		TYPE = tYPE;
-	}
-	
-	@Override
-	public String toString() {
-		return "Sample [ID=" + ID + ", VAL=" + VAL + ", TYPE=" + TYPE + "]";
-	}
 	
 }
